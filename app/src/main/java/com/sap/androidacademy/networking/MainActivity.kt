c48f365bc9390b687a13a14bef2ac2d007460b51
Radovan Murin <radovan.murin@sap.com>
2021-06-21 20:17:04 +0200

    Initial state

--- app/src/main/java/com/sap/androidacademy/networking/MainActivity.kt
+++ app/src/main/java/com/sap/androidacademy/networking/MainActivity.kt
@@ -2,24 +2,39 @@ package com.sap.androidacademy.networking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var volleyQueue: RequestQueue
 
     companion object {
         val BASE_URL = "https://simple-server.cfapps.eu10.hana.ondemand.com/"
         val RAW_ENDPOINT = "raw"
         val JSON_ENDPOINT = "json"
         val ATTACHMENT_ENDPOINT = "file"
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        volleyQueue = Volley.newRequestQueue(this)
+
+        findViewById<Button>(R.id.retrieve_data_btn).setOnClickListener {
+            retrieveData()
+        }
 
 
     }
 
     fun retrieveData() {
-
     }
 }
