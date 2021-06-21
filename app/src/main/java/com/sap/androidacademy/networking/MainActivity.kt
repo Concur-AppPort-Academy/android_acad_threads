256389c0442c8248e9a477cfb59e9073facd517d
Radovan Murin <radovan.murin@sap.com>
2021-06-21 20:41:34 +0200

    Initial state

--- app/src/main/java/com/sap/androidacademy/networking/MainActivity.kt
+++ app/src/main/java/com/sap/androidacademy/networking/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import com.android.volley.RequestQueue
-import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
