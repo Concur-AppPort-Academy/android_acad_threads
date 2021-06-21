5cdc44490f88d2a29de3a12fe6f3e747f8cf2fcc
Radovan Murin <radovan.murin@sap.com>
2021-06-21 20:01:57 +0200

    Initial state

--- simple-server/index.js
+++ simple-server/index.js
@@ -1,19 +1,38 @@
 var express = require("express");
+
+var index = 0;
+
+var listOfPeople = [
+  {
+    name: "Sarah",
+    surname: "Connor",
+    favouriteDay: "favorite_day",
+  },
+];
+
 var app = express();
 app.listen(8080, () => {
- console.log("Server running on port 3000");
+  console.log("Server running on port 8080");
 });
 
 app.get("/json", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
- });
+  setTimeout(function () {
+    index = (index + 1) % listOfPeople.length;
+    res.json(listOfPeople[index]);
+  }, 1500);
+});
 
- app.get("/raw", (req, res, next) => {
-    res.send("AHOJ")
- });
+app.get("/raw", (req, res, next) => {
+  setTimeout(function () {
+    res.attachment();
+    res.sendFile("/tmp/academy_networking/simple-server/resources/image.jpeg");
+  }, 1500);
+  res.send("AHOJ");
+});
 
-
- app.get("/file", (req, res, next) => {
-    res.attachment()
-    res.sendFile("/tmp/academy_networking/simple-server/resources/image.jpeg")
- });
+app.get("/file", (req, res, next) => {
+  setTimeout(function () {
+    res.attachment();
+    res.sendFile("/tmp/academy_networking/simple-server/resources/image.jpeg");
+  }, 1500);
+});
